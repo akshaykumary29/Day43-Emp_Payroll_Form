a3e98897f4ad7d3f1cb4983439fafb8cff09dcc5
akshay <akshay.yamgar29@gmail.com>
2021-12-10 21:01:59 +0530

    Added: New Attribute and Getter Setter methods

--- js/payroll_form_script.js
+++ js/payroll_form_script.js
@@ -58,36 +58,6 @@ class EmployeePayrollData {
     }
 }
 
-function save() {
-    let employeePayrollData = new EmployeePayrollData();
-    try {
-        employeePayrollData.name = document.querySelector("#name").value;
-    } catch (error) {
-        alert(error);
-        return;
-    }
-    employeePayrollData.gender = document.querySelector("#male").checked ? "M" : "F";
-    employeePayrollData.salary = document.querySelector("#salary").value;
-    dateString = document.querySelector("#month").value + " " + document.querySelector("#day").value + ", " + document.querySelector("#year").value;
-    try {
-        employeePayrollData.startDate = new Date(dateString);
-    } catch (error) {
-        alert(error);
-        return;
-    }
-    let departmentsArray = [];
-    document.querySelectorAll("[name=department]").forEach(input => {
-        if (input.checked) departmentsArray.push(input.value);
-    });
-    try {
-        employeePayrollData.departments = departmentsArray;
-    } catch (error) {
-        alert(error);
-        return;
-    }
-    alert("Employee Added Successfully!\n" + employeePayrollData.toString());
-}
-
 const salary = document.querySelector("#salary");
 const output = document.querySelector(".salary-output");
 salary.oninput = function() {
